28429cf3c39b0d452cfc792d91ae36ae43e70535
Simon Ramstedt <simonramstedt@gmail.com>
2019-10-22 12:14:33 -0400

    Update mlagents

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setup(name='avenue',
             # "mlagents==0.5.0",
             "gym",
             # "mlagents_frozen",
-            "mlagents @ git+https://git@github.com/rmst/ml-agents-frozen@e680d51306d7ab593659d70de6efd1d32012a314#egg=mlagents",
+            "mlagents @ git+https://git@github.com/rmst/ml-agents-frozen@0642510f1d61299ee866c953ae67cbfece85c2c4#egg=mlagents",
       ],
       extras_require={},
       packages=find_packages()
