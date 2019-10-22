fb1d9a2cd0dc78d1d4f41c13f3c0e44633f510b5
Simon Ramstedt <simonramstedt@gmail.com>
2019-10-21 22:45:47 -0400

    Hash mlagents dependency

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setup(name='avenue',
             # "mlagents==0.5.0",
             "gym",
             # "mlagents_frozen",
-            "mlagents @ git+https://git@github.com/rmst/ml-agents-frozen#egg=mlagents",
+            "mlagents @ git+https://git@github.com/rmst/ml-agents-frozen@93f5321c117d2c6865eb51b3c8a626157dca2fd8#egg=mlagents",
       ],
       extras_require={},
       packages=find_packages()
